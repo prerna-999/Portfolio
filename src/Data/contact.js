98f0eb47a4e7fe1671e62733a95a318932300765
Prerna <prernathakur3821@gmail.com>
2025-06-22 04:13:38 +0530

    delete info

--- src/Data/contact.js
+++ src/Data/contact.js
@@ -1,7 +1,1 @@
-export const contact = {
-  phone: "+91 8219015966",
-  email: "prernathakur9726@gmail.com",
-  address: "Guru Teg Bahadur Nagar, Street No. 5, House No. 2943, Kharar",
-  linkedin: "https://www.linkedin.com/in/prerna-thakur-16aa24305"
-};
 
